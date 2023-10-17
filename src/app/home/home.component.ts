2eeda6a7cd8ffdbb1707957d2f50d6e9f3a494b8
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-10-17 12:32:14 +0200

    no consigo frenar el texto dinámico

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   faArrowDown = faArrowDown;
+
+  textList: string[] = [
+    'Front Developer',
+    'Back Developer',
+    'Api creator',
+    'Basketball player',
+  ];
+
+  currentIndex: number = 0;
+  currentText: string = this.textList[0];
+
+  ngOnInit() {
+    interval(3000).subscribe(() => this.changeText());
+  }
+
+  changeText() {
+    this.currentIndex = (this.currentIndex + 1) % this.textList.length;
+    setTimeout(() => {
+      this.currentText = this.textList[this.currentIndex];
+      document.querySelector('.dynamic-text')?.classList.add('hidden');
+      setTimeout(() => {
+        document.querySelector('.dynamic-text')?.classList.remove('hidden');
+      }, 500);
+    }, 500);
+  }
 }
